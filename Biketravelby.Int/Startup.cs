5c180121a2d10714e3fd1d035970369d80ec3018
Yegor Kozlovich <k1ppregel@gmail.com>
2016-04-05 23:14:42 +0300

    Initial commit

---
+++ Biketravelby.Int/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Biketravelby.Int.Startup))]
+namespace Biketravelby.Int
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
