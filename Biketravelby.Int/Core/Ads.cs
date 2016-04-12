b3ec46ffcc87fc40d659802ba1962f773f8d29b0
Yegor Kozlovich <xkoz@ericpol.com>
2016-04-12 15:46:23 +0300

    Controller to managing ads

---
+++ Biketravelby.Int/Core/Ads.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biketravelby.Int.Core
+{
+    public class Ads
+    {
+        public int AdsID { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime Finish { get; set; }
+
+        public double? Price { get; set; }
+
+        public string Comment { get; set; }
+    }
+
+}
---
+++ Biketravelby.Int/DAL/DbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Biketravelby.Int.Core;
+
+namespace Biketravelby.Int.DAL
+{
+    
+    public class BiketravelbyIntDbContext : DbContext
+    {
+        public BiketravelbyIntDbContext() : base("DefaultConnection") { }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Passport> Passport { get; set; }
+        public DbSet<Visa> Visa { get; set; }
+
+        public DbSet<Ads> Ads { get; set; }
+
+    }
+}
